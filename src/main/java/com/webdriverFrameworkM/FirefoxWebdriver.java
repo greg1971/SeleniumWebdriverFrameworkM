a16472ce1c08511687d46aa7f2801d5943b80b60
greg1971 <gregory.kavadias@gmail.com>
2015-12-17 22:31:42 +0200

    FirefoxWebdriver.java - Firefox profile

--- src/main/java/com/webdriverFrameworkM/FirefoxWebdriver.java
+++ src/main/java/com/webdriverFrameworkM/FirefoxWebdriver.java
@@ -3,6 +3,7 @@ package com.webdriverFrameworkM;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 /** Class that instantiates and returns a FirefoxDriver object to be used for testing a 
  * web application. 
 * @author Gregorio Kavadias
@@ -16,8 +17,11 @@ public class FirefoxWebdriver implements SeleniumWebdriver{
 	 * @return			returns a org.openqa.selenium.firefox.FirefoxDriver.FirefoxDriver() object
 	 */
 	public FirefoxDriver createWebdriver() {
+		FirefoxProfile profile = new FirefoxProfile();
+		profile.setPreference("browser.startup.homepage", "about:blank");
+		profile.setPreference("startup.homepage_welcome_url.additional", "about:blank");
 		
-		driver = new FirefoxDriver();
+		driver = new FirefoxDriver(profile);
 		driver.manage().timeouts().implicitlyWait(WebdriverUtilities.TIMOUT_WAITSEC, TimeUnit.SECONDS);
 		
 		return driver;
